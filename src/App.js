64712dfee81ef76a972e1497e041e970aa398a81
fmebsnews <fmebsnews@fmebs.com>
2023-11-03 17:29:54 +0900

    최종 완성 코드

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
+import Navigation from './components/Navigation';
+import Detail from './routes/Detail';
 
 function App() {
-  return <div>hi~</div>;
+  return (
+    <BrowserRouter>    
+        <Navigation />  
+        <Routes>          
+          <Route path='/' Component={Home} />
+          <Route path='/about' Component={About} />
+          <Route path='/movie-detail' Component={Detail} />
+        </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
